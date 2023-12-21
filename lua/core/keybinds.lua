d0e522752a3b0e90f3a47a0deb2a47783ec37628
David-Alexandre Lapierre <davidalexlapierre@gmail.com>
2023-12-21 10:00:04 -0500

    added some keybinds nits

--- lua/core/keybinds.lua
+++ lua/core/keybinds.lua
@@ -1,5 +1,13 @@
 
 
 -- Terminal
-
 vim.api.nvim_set_keymap('n', '<M-q>', ':split<CR>:wincmd j<CR>:term<CR>i', { noremap = true, silent = true })
+
+
+-- Window control
+vim.keymap.set('n', '<M-Left>', ':wincmd h<CR>')
+vim.keymap.set('n', '<M-Right>', ':wincmd l<CR>')
+vim.keymap.set('n', '<M-Up>', ':wincmd k<CR>')
+vim.keymap.set('n', '<M-Down>', ':wincmd j<CR>')
+
+

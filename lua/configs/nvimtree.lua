fb35409362eacd2ad074c061e31ef9b2ced82898
David Alexandre Lapierre <dlapierre@microsoft.com>
2023-12-22 11:20:50 -0500

    added some stuff

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -17,3 +17,5 @@ require("nvim-tree").setup({
 })
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
+vim.keymap.set('n', '<c-m>', ':NvimTreeFindFile<CR>')
+vim.keymap.set('n', '<c-\\>', ':NvimTreeCollapse<CR>')
--- lua/core/editor.lua
+++ lua/core/editor.lua
@@ -12,3 +12,5 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.shiftround = true
 vim.opt.expandtab = true
+
+vim.wo.relativenumber = true
